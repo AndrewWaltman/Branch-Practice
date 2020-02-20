c0f9326149ce6356a323bb6cc3bd98758575718b
AndrewWaltman <andrew.w.waltman@gmail.com>
2020-02-19 20:50:29 -0500

    Branch practice.

---
+++ Brancing Practice/Brancing Practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Brancing_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
